ecfdc4c030e365ec96ec30fd9d775c51c090a757
Alejandro Colomar <alx@kernel.org>
2025-02-22 20:10:41 +0100

    src/useradd.c: Set 'path' closer to its first use
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- src/useradd.c
+++ src/useradd.c
@@ -2180,7 +2180,6 @@ static void create_home (void)
 	if (access (prefix_user_home, F_OK) == 0)
 		return;
 
-	strcpy(path, "");
 	bhome = strdup(prefix_user_home);
 	if (!bhome) {
 		fprintf(stderr,
@@ -2202,6 +2201,7 @@ static void create_home (void)
 	   exists. If not, create it with permissions 755 and
 	   owner root:root.
 	 */
+	strcpy(path, "");
 	for (cp = strtok(bhome, "/"); cp != NULL; cp = strtok(NULL, "/")) {
 		/* Avoid turning a relative path into an absolute path. */
 		if (strprefix(bhome, "/") || !streq(path, ""))
