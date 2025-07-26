c00f42a1dfeb13e0bf869d3ad077ba49bf79f8f4
Alejandro Colomar <alx@kernel.org>
2025-07-26 12:03:15 +0200

    lib/: Simplify error handling
    Print simpler messages that don't need translation.
    Check libc errors with ==-1 and ==NULL instead of <0 (or !=0) and !p.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- lib/copydir.c
+++ lib/copydir.c
@@ -113,7 +113,7 @@ error_acl(struct error_context *, const char *fmt, ...)
 	}
 
 	va_start (ap, fmt);
-	(void) fprintf (shadow_logfd, _("%s: "), log_get_progname());
+	fprintf(shadow_logfd, "%s: ", log_get_progname());
 	vfprinte(shadow_logfd, fmt, ap);
 	va_end (ap);
 }
