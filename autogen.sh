dae941ff0201632f2e1ca9ce051e68bde01e138d
Alejandro Colomar <alx@kernel.org>
2025-08-07 14:15:42 +0200

    Use a patched Clang

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,7 @@
 
 autoreconf -v -f --install "$(dirname "$0")" || exit 1
 
+CC="/home/alx/src/llvm/llvm/opt/build/bin/clang"
 CFLAGS="-O2"
 CFLAGS="$CFLAGS -Wall"
 CFLAGS="$CFLAGS -Wformat=2"
@@ -147,6 +148,7 @@ CFLAGS="$CFLAGS -Wno-unknown-attributes"
 CFLAGS="$CFLAGS -Wno-unknown-warning-option"
 
 "$(dirname "$0")"/configure \
+	CC="$CC" \
 	CFLAGS="$CFLAGS" \
 	--enable-lastlog \
 	--disable-logind \
